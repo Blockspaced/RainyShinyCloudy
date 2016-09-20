d29f357e64e3a694a08c079f443beab57ebf5d91
smbss <sandro.simes92@gmail.com>
2016-09-20 20:23:22 +0100

    2ndLC-Table View delegate and datasource

--- RainyShinyCloudy/WeatherVC.swift
+++ RainyShinyCloudy/WeatherVC.swift
@@ -10,7 +10,7 @@ import UIKit
 import Alamofire
 import Foundation
 
-class WeatherVC: UIViewController {
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var currentTempLabel: UILabel!
@@ -24,6 +24,22 @@ class WeatherVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        
+        return cell
     }
 
 }
